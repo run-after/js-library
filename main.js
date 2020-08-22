0a617fe0f6bbbdd1adc5094e715554ddff127bd7
run-after <run.after@protonmail.com>
2020-08-22 13:04:48 -0500

    Init and finish 1-4... minor styling

---
+++ main.js
@@ -0,0 +1,71 @@
+let library = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function() {
+    return `${title} by ${author}, ${pages} pages...Read yet: ${read}`;
+  }
+}
+
+function addBookToLibrary(book){
+  library.push(book);
+}
+
+function addCard(book, index) {
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML = `<div class="close">X</div>
+                   <h1>${book.title}</h1>
+                   <h3>${book.author}</h3>
+                   <p>${book.pages} pages</p>
+                   <p>Status: ${book.read}</p>`;
+  div.setAttribute('data-index', index)
+  const cards = document.querySelector('.cards');
+  cards.appendChild(div);
+}
+
+function render(library) {
+  library.forEach(addCard);
+}
+// MODAL
+
+let modal = document.getElementById('modal');
+
+let newBtn = document.getElementById('new-book-btn');
+
+newBtn.onclick = function () {
+  modal.style.display = "block";
+}
+
+function clear() {
+  const cards = document.querySelector('.cards');
+  cards.innerHTML = '';
+}
+
+function createNew() {
+  let title = document.getElementById("title").value;
+  let author = document.getElementById("author").value;
+  let pages = document.getElementById("pages").value;
+  let read = document.forms.addBook.elements.read.value;
+  
+  if(read === "true") {
+    read = 'Read';
+  } else {
+    read = 'Not yet read';
+  }
+
+  let book = new Book(title, author, pages, read);
+  addBookToLibrary(book);
+}
+
+let addBtn = document.getElementById('add-book-btn');
+
+addBtn.onclick = function() {
+  clear();
+  createNew();
+  modal.style.display="none";
+  render(library);
+}
