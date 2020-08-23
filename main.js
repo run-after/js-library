5f134bf501e05acb682f98280f8b47e18706f471
run-after <run.after@protonmail.com>
2020-08-23 09:50:39 -0500

    Commit to ask question

--- main.js
+++ main.js
@@ -17,12 +17,13 @@ function addBookToLibrary(book){
 function addCard(book, index) {
   const div = document.createElement('div');
   div.classList.add('card');
-  div.innerHTML = `<div class="close">X</div>
+  div.classList.add(`card-${index}`);/////////////////////////
+  div.innerHTML = `<div class="close-btn">X</div>
                    <h1>${book.title}</h1>
                    <h3>${book.author}</h3>
                    <p>${book.pages} pages</p>
                    <p>Status: ${book.read}</p>`;
-  div.setAttribute('data-index', index)
+  div.setAttribute('data-index', index);
   const cards = document.querySelector('.cards');
   cards.appendChild(div);
 }
@@ -69,3 +70,22 @@ addBtn.onclick = function() {
   modal.style.display="none";
   render(library);
 }
+
+/////////////////////////////////////////////
+let closeBtn = document.getElementsByClassName('close-btn');
+
+closeBtn.onclick = function () {
+  alert('dog');
+}
+
+/*
+function removeBookFromLibrary(book) {
+  let card = document.querySelector(`[data-index=${}`)
+}
+
+
+
+CLOSE BUTTON NOT DOING SHIT!
+
+
+*/
