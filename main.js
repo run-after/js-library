5d2b1bd743ed88a3e87647cc65df39bd49e8e3f0
run-after <run.after@protonmail.com>
2020-08-23 15:44:01 -0500

    Refactor for readability

--- main.js
+++ main.js
@@ -7,9 +7,6 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
-    return `${title} by ${author}, ${pages} pages...Read yet: ${read}`;
-  }
 }
 
 // Adds book to library array
@@ -17,12 +14,10 @@ function addBookToLibrary(book){
   library.push(book);
 }
 
-// Creates a new card, fills in all the data, adds listener to delete
-// card.
+// Creates a new card, fills in all the data, adds listener to delete card
 function addCard(book, index) {
   const div = document.createElement('div');// Creates new div
-  div.classList.add('card'); // Adds class card
-  div.classList.add(`card-${index}`);// Adds class to help find it (delete this line)
+  div.classList.add('card'); // Adds class card for styling
   div.innerHTML = `<h1>${book.title}</h1> 
                    <h3>${book.author}</h3>
                    <p>${book.pages} pages</p>
@@ -30,11 +25,28 @@ function addCard(book, index) {
   div.setAttribute('data-index', index); // Sets data attribute to find card
   const cards = document.querySelector('.cards');// Selects parent element
   cards.appendChild(div);// Adds card as a child to the end
-  const closeBtn = document.createElement('div'); // Creates a div inside card(close button)
-  closeBtn.classList.add('close-btn');// Adds class to style button
-  closeBtn.textContent = 'X';// Adds letter 'x'
-  div.insertBefore(closeBtn, div.firstChild);// Adds delete button to top of card
+
+  const deleteBtn = document.createElement('div'); // Creates a div inside card(close button)
+  deleteBtn.classList.add('delete-btn');// Adds class to style button
+  deleteBtn.textContent = 'X';// Adds letter 'x'
+  div.insertBefore(deleteBtn, div.firstChild);// Adds delete button to top of card
   addDelete(div);// Adds delete listener
+}
+
+// Creates listener to delete button on each card.
+function addDelete(div){
+  const deleteBtn = div.querySelector('.delete-btn');
+  deleteBtn.onclick = function () {
+    deleteBook(div);
+  }
+}
+
+function deleteBook(book) {
+  // Removes from list
+  book.parentElement.removeChild(book);
+  // Delete from library
+  let index = book.getAttribute('data-index');
+  library.splice(index, 1);
 }
 
 // Loops through each book in library and creates a card for it.
@@ -45,16 +57,10 @@ function render(library) {
 // MODAL (Pop up for creating new book)
 
 let modal = document.getElementById('modal');
-
 let newBtn = document.getElementById('new-book-btn');
-
+// Pops up form
 newBtn.onclick = function () {
   modal.style.display = "block";
-}
-// Deletes all existing cards on site so they are not duplicated
-function clear() {
-  const cards = document.querySelector('.cards');
-  cards.innerHTML = '';
 }
 
 // Create's new book and then adds it to the library
@@ -83,28 +89,8 @@ addBtn.onclick = function() {
   render(library);
 }
 
-// Seeds books into library // 
-/*
-book = new Book('title', 'author', '300', 'true');
-addBookToLibrary(book);
-addBookToLibrary(book);
-addCard(book, 1);
-addCard(book, 2);
-*/
-// Creates listener to delete button on each card.
-function addDelete(div){
-  const closeBtn = div.querySelector('.close-btn');
-  closeBtn.onclick = function () {
-    deleteBook(div);
-  }
-}
-
-function deleteBook(book) {
-
-    // Removes from list
-    book.parentElement.removeChild(book);
-
-    // Delete from library
-    let index = book.getAttribute('data-index');
-    library.splice(index, 1);
+// Deletes all existing cards from DOM so they are not duplicated
+function clear() {
+  const cards = document.querySelector('.cards');
+  cards.innerHTML = '';
 }
